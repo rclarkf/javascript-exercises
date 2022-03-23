9dfe73ae9c829e54a8be24e773b8202b2ad412f2
Richard <rclark2005@gmail.com>
2022-03-23 00:16:38 -0400

    ejercicio 2 RepeatString listo

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(cadena, num) {
+let result="";
+for (let i=0;i<num;i++) {
+    result += cadena;
+}
+return result;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
